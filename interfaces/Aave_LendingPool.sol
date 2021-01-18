8b6e7a09054a6b583afb41b3da6b005065bc9b09
Splidge <73956628+Splidge@users.noreply.github.com>
2021-01-18 18:39:51 +0000

    import Aave interfaces

--- interfaces/Aave_LendingPool.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
-
-//@Created by Daniel Chilvers
-interface LendingPool {
-
-    function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
-    function withdraw(address asset, uint256 amount, address to) external;
-}
