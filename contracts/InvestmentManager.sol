cef109accb28cd36564e78b6003041ec92ac667c
Splidge <73956628+Splidge@users.noreply.github.com>
2021-01-21 12:12:02 +0000

    Adding Approvals

--- contracts/InvestmentManager.sol
+++ contracts/InvestmentManager.sol
@@ -5,6 +5,10 @@ pragma experimental ABIEncoderV2;
 import "interfaces/ILendingPool.sol";
 import "interfaces/IProtocolDataProvider.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/drafts/ERC20Permit.sol"; 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/SafeCast.sol";
 
   /**
    * @title Manages depositing and withdrawing from Aave
@@ -12,6 +16,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
    * @author Daniel Chilvers
    **/
 contract InvestmentManager is Ownable {
+
+    using SafeMath for uint256;
+
     //address internal kovanLendingPool = 0x9FE532197ad76c5a68961439604C037EB79681F0;
     //address internal mainLendingPool = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9;
     address internal deadAddress = 0x000000000000000000000000000000000000dEaD;
@@ -31,19 +38,21 @@ contract InvestmentManager is Ownable {
     /// @dev TO;DO change to saving the asset symbol instead 
     /// @dev then this will be packed and we save gas
     struct User {
-        address asset;
-        uint128 balance;
+        address[] asset;
+        uint128[] balance;
+        uint128[] allowance;
         bool locked;
     }
 
     /// @dev map user addresses to our struct above
-    mapping (address => User[]) Users;
+    mapping (address => User) Users;
 
     IProtocolDataProvider.TokenData[] public aTokens;
 
     ILendingPool lendingPool;
     ILendingPoolAddressesProvider lendingPoolAddressesProvider;
     IProtocolDataProvider protocolDataProvider;
+    IERC20 erc20;
 
     constructor(uint _networkID) {
         /// @dev truffle will pass in the networkID when migrated, use this to select the correct address
@@ -57,36 +66,98 @@ contract InvestmentManager is Ownable {
         protocolDataProvider = IProtocolDataProvider(lendingPoolAddressesProvider.getAddress(bytes32('0x1')));
     }
 
-    event debug(address _address);
+    event debug(address _address, uint256 state);
+
+    function approveDeposit(uint128 _amount, address _asset) public returns(bool) {
+        uint256 _index = 0;
+        bool _success;
+        erc20 = IERC20(_asset);
+        /// @dev check if the user already has some of this asset
+        for (uint i; i <= Users[msg.sender].asset.length ; i++) {
+            if (Users[msg.sender].asset[i] == _asset ){
+                _index = i;
+            }else {
+                /// @dev they don't own any yet, lets add a record
+                Users[msg.sender].asset.push();
+                Users[msg.sender].balance.push();
+                Users[msg.sender].allowance.push();
+                emit debug(msg.sender, 2);
+            }
+        }
+        _success = erc20.approve(msg.sender, _amount);
+        Users[msg.sender].asset[_index] = _asset;
+        Users[msg.sender].allowance[_index] = safeAddUint128(Users[msg.sender].allowance[_index], _amount );
+        emit debug(msg.sender, 3);
+    }
 
 
-    function deposit(address _reserve, uint128 _amount) public payable{
-        uint _index = 0;
-        /// @dev check if the user already has some of this asset   
-        for (uint i; i <= Users[msg.sender].length ; i++) {
-            if (Users[msg.sender][i].asset == _reserve ){
+
+    function deposit(address _reserve, uint128 _amount) public {
+        uint256 _index = 0;
+
+        for (uint i; i <= Users[msg.sender].asset.length ; i++) {
+            if (Users[msg.sender].asset[i] == _reserve ){
                 _index = i;
             } else {
-                Users[msg.sender].push();
+                /// @dev they must not have approved yet.
+                emit debug(msg.sender, 1);
             }
             lendingPool.deposit(_reserve, _amount, address(this) , referralCode);
-            Users[msg.sender][_index].balance = Users[msg.sender][_index].balance + _amount;
+            Users[msg.sender].balance[_index] = safeAddUint128(Users[msg.sender].balance[_index], _amount );
         }
+    }
+
+    function approveWithdraw(uint128 _amount, address _asset) public returns(bool) {
+        uint256 _index = 0;
+        bool _success;
+        address aToken;
+        (aToken,,) = protocolDataProvider.getReserveTokensAddresses(_asset);
+        erc20 = IERC20(aToken);
+        /// @dev check if the user has some of this asset to withdraw
+        for (uint i; i <= Users[msg.sender].asset.length ; i++) {
+            if (Users[msg.sender].asset[i] == _asset ){
+                _index = i;
+            }else {
+                /// @dev they don't own any
+                //Users[msg.sender].asset.push();
+                //Users[msg.sender].balance.push();
+                //Users[msg.sender].allowance.push();
+                emit debug(msg.sender, 4);
+                _success = false;
+                return (_success);
+            }
+        }
+        Users[msg.sender].asset[_index] = _asset;
+        Users[msg.sender].allowance[_index] = safeSubUint128(Users[msg.sender].allowance[_index], _amount );
+        _success = erc20.approve(msg.sender, _amount);
+        emit debug(msg.sender, 5);
+        return(_success);
     }
 
     function withdraw(address _reserve, uint128 _amount) public {
         uint _index = 0;
         bool _owned = false;
-        for (uint i; i <= Users[msg.sender].length ; i++) {
-            if (Users[msg.sender][i].asset == _reserve ){
+        for (uint i; i <= Users[msg.sender].asset.length ; i++) {
+            if (Users[msg.sender].asset[i] == _reserve ){
                 _index = i;
                 _owned = true;
             }
         }
-        if (_owned = true || Users[msg.sender][_index].balance >= _amount){
+        if (_owned = true && Users[msg.sender].balance[_index] >= _amount){
             lendingPool.withdraw(_reserve, _amount, msg.sender);
+        } else {
+            emit debug(msg.sender, 6);
         }
         
+    }
+
+    /// @notice uses the SafeMath and SafeCast libraries to safely add uint128s together
+    function safeAddUint128(uint128 _a, uint128 _b) internal pure returns(uint128){
+        return(SafeCast.toUint128(uint256(_a).add(uint256(_b))));
+    }
+    /// @notice uses the SafeMath and SafeCast libraries to safely subtract uint128s 
+    function safeSubUint128(uint128 _a, uint128 _b) internal pure returns(uint128){
+        return(SafeCast.toUint128(uint256(_a).sub(uint256(_b))));
     }
 
     /// @notice don't send me funds if this function still exists
@@ -113,7 +184,7 @@ contract InvestmentManager is Ownable {
     /// @notice update the lending pool address from the address provider
     function updateLendingPoolAddress() public onlyOwner returns(address){
         lendingPooladdress = lendingPoolAddressesProvider.getLendingPool();
-        emit debug(lendingPooladdress);
+        emit debug(lendingPooladdress, 10);
         return (lendingPooladdress);
     }
 
