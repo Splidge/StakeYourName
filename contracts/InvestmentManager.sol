302a5363e704ebb9814ce69603597e49132ba1b7
Splidge <73956628+Splidge@users.noreply.github.com>
2021-01-22 20:18:09 +0000

    Interest calculation incorrect

--- contracts/InvestmentManager.sol
+++ contracts/InvestmentManager.sol
@@ -35,19 +35,38 @@ contract InvestmentManager is Ownable {
     address internal USDCContract = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     uint16 referralCode = 0;
-    uint256 constant MAX_INT = 2**256 - 1;
+    uint256 constant MAX_INT = type(uint256).max;
 
-    /// @dev TO:DO change to saving the asset symbol instead 
-    /// @dev then this will be packed and we save gas
-    /// @dev TO:DO change arrays to mappings to remove for loops 
+    /// @notice User data, maps an assest contract address to their balance, interest earnt
+    /// @notice and if they've locked the original investment (no auto-switching, no loss investement)
+    /// @dev we can increase balance to uint256, changing to mappings removed the packing benefits
     struct User {
         mapping (address => uint128) balance;
-        mapping (address => uint128) allowance;
+        mapping (address => uint128) interest;
         mapping (address => bool) locked;
     }
 
     /// @dev map user addresses to our struct above
     mapping (address => User) Users;
+
+    /// @dev balance, how many of each token bought
+    /// @dev interest, how much interest earnt so far
+    /// @dev interestCalcualtionTime, block.timestamp of last calculation
+    /// @dev TO:DO pack this struct better
+    struct Asset {
+        uint256 balance;
+        uint256 interest;
+        //uint256 interestCalculationTime;
+        address[] userList;
+    }
+
+    mapping (address => Asset) assets;
+
+
+    /// @dev save a list of any assests we've puchased
+    address[] public assetList;
+    /// @dev save a list of users that have purchased asset
+    address[] public userList;
 
     IProtocolDataProvider.TokenData[] public aTokens;
 
@@ -57,69 +76,193 @@ contract InvestmentManager is Ownable {
     IERC20 ierc20;
 
     constructor(uint _networkID) {
-        /// @dev truffle will pass in the networkID when migrated, use this to select the correct address
+        /// @dev truffle will pass in the networkID when migrated, use this to select the correct addresses
+        /// @dev uprgading to ^0.8.0 we could use block.chainid instead
         if (_networkID == 1){
             lendingPoolAddressProviderAddress = mainLendingPoolAddressProviderAddress;
+            protocolDataProvider = IProtocolDataProvider(0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d);
         } else if (_networkID == 42){
             lendingPoolAddressProviderAddress = kovanLendingPoolAddressProviderAddress;
+            protocolDataProvider = IProtocolDataProvider(0x3c73A5E5785cAC854D468F727c606C07488a29D6);
         } 
         lendingPoolAddressesProvider = ILendingPoolAddressesProvider(lendingPoolAddressProviderAddress);
         lendingPooladdress =  lendingPoolAddressesProvider.getLendingPool();
-        protocolDataProvider = IProtocolDataProvider(lendingPoolAddressesProvider.getAddress(bytes32('0x1')));
+        //protocolDataProvider = IProtocolDataProvider(lendingPoolAddressesProvider.getAddress(bytes32('0x1')));
         lendingPool = ILendingPool(lendingPooladdress);
     }
 
     event debug(address _address, uint256 state);
 
+    /// @notice Approves sending _asset ERC20 tokens to the lendingPool
+    /// @dev this needs to be called for each new asset purchased
     function approveLendingPool(address _asset) public {
         ierc20 = IERC20(_asset);
         ierc20.approve(lendingPooladdress , MAX_INT);
     }
 
-    /// @notice just does a deposit, don't record to a users balance
+    function userBalance(address _asset) public view returns(uint128){
+        return(Users[msg.sender].balance[_asset]);
+    }
+
+    function userInterest(address _asset) public view returns(uint128){
+        return(Users[msg.sender].interest[_asset]);
+    }
+
+    /// @notice calculates how many extra aTokens we have i.e. interest
+    /// @notice divides and adds result to users balances
+    /// @dev do we need to collect the dust and distribute that later?
+    function calculateInterest(address _asset) public {
+        //require(assets[_asset].balance != 0);
+        address _aTokenAddress;
+        (_aTokenAddress,,) = protocolDataProvider.getReserveTokensAddresses(_asset);
+        //_aTokenAddress = 0xdCf0aF9e59C002FA3AA091a46196b37530FD48a8;
+        ierc20 = IERC20(_aTokenAddress);
+        uint256 _interest = ierc20.balanceOf(address(this));
+        _interest = _interest.sub(assets[_asset].balance);
+        _interest = _interest.sub(assets[_asset].interest);
+        uint256 _interestSplit = _interest.div(assets[_asset].userList.length);
+        /// @dev we want to leave any dust behind, so mod the _interest before updating the asset interest balance
+        /// @dev this is fine because the dust will be collected the next time interest is calculated
+        _interest = _interest.mod(assets[_asset].userList.length);
+        assets[_asset].interest = assets[_asset].interest.add(_interest);
+        for (uint i; i < assets[_asset].userList.length; i++){
+            Users[address(assets[_asset].userList[i])].interest[_asset] = SafeCast.toUint128(uint256(Users[address(assets[_asset].userList[i])].interest[_asset]).add(_interestSplit));
+        }
+    }
+
+    /// @notice just does a deposit, doesn't record to any users balance
     /// @dev just for testing, remove later
     function depositTest(address _asset, uint256 _amount) public {
         ierc20 = IERC20(_asset);
         ierc20.transferFrom(msg.sender, address(this), _amount);
         lendingPool.deposit(_asset, _amount, address(this) , referralCode);
+        assets[_asset].balance = assets[_asset].balance.add(_amount);
     }
 
     /// @notice just does a withdrawl, doesn't record to a users balance
     /// @dev just for testing, remove later
-    function withdrawTest(address _asset, uint256 _amount) public {
+    function withdrawTest(address _asset, uint256 _amount) public onlyOwner {
         lendingPool.withdraw(_asset, _amount, msg.sender);
     }
 
-    /// @notice approves an _asset transfer for an _amount and records it
-    function approveDeposit(address _asset, uint128 _amount) public returns(bool) {
+
+    /// @dev this isn't how it works, the user needs to approve directly to the asset contract
+    function approveDeposit(address _asset, uint128 _amount) public {
+        require(_asset != address(0));
+        require(_amount > 0);
         ierc20 = IERC20(_asset);
         ierc20.approve(address(this), _amount);
-        Users[msg.sender].allowance[msg.sender] =  _amount;
+        if (!checkAssetList(_asset)) {
+            addAsset(_asset);
+        }
     }
 
-
-    function checkAllowance (address _asset) public view returns(uint128){
-        return Users[msg.sender].allowance[_asset];
+    /// @notice checks if we've purchased this assest before, make internal later
+    function checkAssetList(address _asset) public view returns(bool){
+        for (uint i; i < assetList.length; i++ ){
+            if(address(assetList[i]) == _asset){
+                return true;
+            }
+        }
+        return false;
     }
 
-    /// @dev can we make the deposit a require? to save gas.
+    /// @notice adds an _asset to the assetList and approves sending to the lendingPool
+    function addAsset(address _asset) public onlyOwner{
+        assetList.push(_asset);
+        approveLendingPool(_asset);
+        //approveATokenBurn(_asset, MAX_INT);
+    }
+
+    /// @notice checks if this user has purchased this asset previously
+    function checkUserList(address _asset) public view returns(bool){
+        for (uint i; i < assets[_asset].userList.length; i++ ){
+            if(assets[_asset].userList[i] == msg.sender){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// @notice records that the user has now purchased _asset
+    function addUser(address _asset) public onlyOwner{
+        assets[_asset].userList.push(msg.sender);
+        //approveLendingPool(_asset);
+        //approveATokenBurn(_asset, MAX_INT);
+    }
+
+    function checkAllowance (address _asset) public returns(uint256){
+        ierc20 = IERC20(_asset);
+        return ierc20.allowance(msg.sender, address(this));
+    }
+
+    /// @dev TO:DO add some more requires to potentially save gas
     function deposit(address _asset, uint128 _amount) public {
-        require(checkAllowance(_asset) >= Users[msg.sender].balance[_asset], "User not approved to send this amount");
+        require(_asset != address(0));
+        require(checkAllowance(_asset) >= _amount, "User not approved to send this amount");
+        require(_amount > 0, "Deposit must be non-zero");
+        if (!checkAssetList(_asset)) {
+            addAsset(_asset);
+        }
+        ierc20 = IERC20(_asset);
+        ierc20.transferFrom(msg.sender, address(this), _amount);
         lendingPool.deposit(_asset, _amount, address(this) , referralCode);
+        assets[_asset].balance = assets[_asset].balance.add(_amount);
+        if (!checkUserList(_asset)) {
+            addUser(_asset);
+        }
         Users[msg.sender].balance[_asset] = safeAddUint128(Users[msg.sender].balance[_asset], _amount );
     }
 
-    /// @notice allow the lendingPool to burn _amount of aTokens in order to withdraw _asset
-    function approveATokenBurn(uint128 _amount, address _asset) public {
-        ierc20 = IERC20(_asset);
+    /// @dev don't think we need to do this!?
+    function approveATokenBurn(address _asset, uint256 _amount) public {
+        address _aTokenAddress;
+        (_aTokenAddress,,) = protocolDataProvider.getReserveTokensAddresses(_asset);
+        ierc20 = IERC20(_aTokenAddress);
         ierc20.approve(lendingPooladdress , _amount);
     }
 
-    ///@dev this needs updating so users can withdraw interest also
+    ///@dev current implemnetation withdraws balance before deposit.
     function withdraw(address _asset, uint128 _amount) public {
-        require(Users[msg.sender].balance[_asset] >= _amount, "Attempting to withdraw too much");
-        lendingPool.withdraw(_asset, _amount, msg.sender);
-        Users[msg.sender].balance[_asset] = safeSubUint128(Users[msg.sender].balance[_asset], _amount);
+        require(_asset != address(0));
+        require(_amount > 0);
+        calculateInterest(_asset);
+        uint128 _maxWithdrawl = safeAddUint128(Users[msg.sender].balance[_asset],Users[msg.sender].interest[_asset]);
+        uint128 _withdrawl = 0;
+        if (_amount > _maxWithdrawl) {
+            Users[msg.sender].balance[_asset] = 0;
+            Users[msg.sender].interest[_asset] = 0;
+            _withdrawl = _maxWithdrawl;
+        } else if (_amount > Users[msg.sender].balance[_asset]) {
+            Users[msg.sender].interest[_asset] = safeSubUint128(Users[msg.sender].interest[_asset],safeSubUint128(_amount, Users[msg.sender].balance[_asset]));
+            Users[msg.sender].balance[_asset] = 0;
+            _withdrawl = _amount;
+        } else {
+            Users[msg.sender].balance[_asset] = safeSubUint128(Users[msg.sender].balance[_asset], _amount);
+            _withdrawl = _amount;
+        }
+        lendingPool.withdraw(_asset, _withdrawl, msg.sender);
+        assets[_asset].balance = assets[_asset].balance.sub(_amount);
+    }
+
+    function convertInterestToBalance(address _asset, uint128 _amount) public {
+        require(_asset != address(0));
+        require(_amount > 0);
+        calculateInterest(_asset);
+        uint128 _check = userTotal(_asset);
+        if (_amount > Users[msg.sender].interest[_asset]){
+            Users[msg.sender].balance[_asset] = safeAddUint128(Users[msg.sender].balance[_asset], Users[msg.sender].interest[_asset]);
+            Users[msg.sender].interest[_asset] = 0;    
+        } else {
+            Users[msg.sender].interest[_asset] = safeSubUint128(Users[msg.sender].interest[_asset], _amount);
+            Users[msg.sender].balance[_asset] = safeAddUint128(Users[msg.sender].balance[_asset], _amount);
+        }
+        assert(_check == userTotal(_asset));
+    }
+
+    /// @dev only call this after calculateInterest()
+    function userTotal(address _asset) public view returns(uint128) {
+        return(safeAddUint128(Users[msg.sender].balance[_asset], Users[msg.sender].interest[_asset]));
     }
 
     /// @notice uses the SafeMath and SafeCast libraries to safely add uint128s together
@@ -150,6 +293,21 @@ contract InvestmentManager is Ownable {
     /// @dev maybe try and shorten the function name a bit
     function checkLendingPoolAddressProviderAddress() public view returns(address){
         return (lendingPoolAddressProviderAddress);
+    }
+    
+    function checkProtocolDataProvider() public view returns(address){
+        return (address(protocolDataProvider));
+    }
+
+    function updateProtocolDataProvider(bytes1 _bytes) public returns(address, bytes32){
+        protocolDataProvider = IProtocolDataProvider(lendingPoolAddressesProvider.getAddress(_bytes));
+        return (address(protocolDataProvider),_bytes);
+    }
+
+    function checkaTokenAddress(address _asset) public view returns(address){
+        address _aTokenAddress;
+        (_aTokenAddress,,) = protocolDataProvider.getReserveTokensAddresses(_asset);
+        return (address(_aTokenAddress));
     }
 
     /// @notice update the lending pool address from the address provider
